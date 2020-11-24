72d8106e0cc7c53062347b88be9b62b98087f190
kumar <kumarkhandagle33@gmail.com>
2020-11-24 11:16:42 +0530

    Clock_1 Assignment

---
+++ Clock_1/top.v
@@ -0,0 +1,20 @@
+module top(
+input clk, 			///Input Clock Source 10 nSec
+output reg clkout); ////Clkout declare as temp
+  
+integer temp;  ///////Counter Variable for Clock Generation
+  
+initial begin   ////Initialization of temp, clkout
+  	temp = 0;
+    clkout = 0;
+end
+  
+//////Start writing your Logic from here  
+
+  
+  
+  
+  
+  
+/////End Your Code Here  
+endmodule
